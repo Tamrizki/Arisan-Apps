1871b410ed1cda25af5384a21084121bd05c3758
Tamrizki <rizkitam77@gmail.com>
2021-01-18 21:52:02 +0700

    create group page

--- app/src/main/java/tam/pa/arisanapps/activity/home/HomeActivity.kt
+++ app/src/main/java/tam/pa/arisanapps/activity/home/HomeActivity.kt
@@ -1,14 +1,18 @@
 package tam.pa.arisanapps.activity.home
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import tam.pa.arisanapps.R
+import tam.pa.arisanapps.activity.creategroup.CreateGroupActivity
 import tam.pa.arisanapps.activity.home.adapter.ListGroupAdapter
 import tam.pa.arisanapps.model.DataListGroup
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), View.OnClickListener {
     var listGroup: ArrayList<DataListGroup> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +23,19 @@ class HomeActivity : AppCompatActivity() {
         rvListGroup.setHasFixedSize(true)
         rvListGroup.layoutManager = LinearLayoutManager(this)
         rvListGroup.adapter = ListGroupAdapter(listGroup, this)
+
+        btnCreateGroup.setOnClickListener(this)
     }
 
     fun setDataArray(){
         for (num in 1..10){
             listGroup.add(DataListGroup("1", "group name 1", "Bulanan", "10", ""))
+        }
+    }
+
+    override fun onClick(view: View?) {
+        if (view == btnCreateGroup){
+            startActivity(Intent(this, CreateGroupActivity::class.java))
         }
     }
 }
